eea0d194895e480b18baed606d005192cc9131a9
qlhan <qlhan@incarcloud.com>
2021-12-30 11:35:15 +0800

    0.1.0 hello world

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < argc; i++)
+    {
+        cout << argv[i] << endl;
+    }
+
     cout << "hello world" << endl;
     return 0;
 }
