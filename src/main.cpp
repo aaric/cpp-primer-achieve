c190c5ad8898b191e850d5560dddef7c76b23324
qlhan <qlhan@incarcloud.com>
2022-02-11 10:10:27 +0800

    0.8.0 main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,38 +6,6 @@ extern int g_two;
 
 int main(int argc, char **argv)
 {
-    // Hello World
-    std::cout << "hello world" << std::endl;
-
-    // C++ version
-    switch (__cplusplus)
-    {
-    case 199711L:
-        std::cout << "C++98" << std::endl;
-        break;
-    case 201103L:
-        std::cout << "C++11" << std::endl;
-        break;
-    case 201402L:
-        std::cout << "C++14" << std::endl;
-        break;
-    case 201703L:
-        std::cout << "C++17" << std::endl;
-        break;
-    case 202002L:
-        std::cout << "C++20" << std::endl;
-        break;
-    default:
-        std::cout << "C++XX" << std::endl;
-        break;
-    }
-
-    // Command line parameters
-    for (int i = 0; i < argc; i++)
-    {
-        std::cout << *argv[i] << std::endl;
-    }
-
     // Tow class
     // std::unique_ptr<Two> two(new Two(10, 20));
     auto two = std::make_unique<Two>(10, 20);
@@ -45,6 +13,38 @@ int main(int argc, char **argv)
     std::cout << "Two: max = " << two->max()
               << ", g_two: " << g_two
               << std::endl;
+
+    // Command line parameters
+    // for (int i = 0; i < argc; i++)
+    // {
+    //     std::cout << *argv[i] << std::endl;
+    // }
+
+    // C++ version
+    // switch (__cplusplus)
+    // {
+    // case 199711L:
+    //     std::cout << "C++98" << std::endl;
+    //     break;
+    // case 201103L:
+    //     std::cout << "C++11" << std::endl;
+    //     break;
+    // case 201402L:
+    //     std::cout << "C++14" << std::endl;
+    //     break;
+    // case 201703L:
+    //     std::cout << "C++17" << std::endl;
+    //     break;
+    // case 202002L:
+    //     std::cout << "C++20" << std::endl;
+    //     break;
+    // default:
+    //     std::cout << "C++XX" << std::endl;
+    //     break;
+    // }
+
+    // Hello World
+    // std::cout << "hello world" << std::endl;
 
     return 0;
 }
