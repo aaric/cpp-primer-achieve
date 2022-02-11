76ee5a27bc96369a7661e35b2d1346401716b5be
qlhan <qlhan@incarcloud.com>
2022-02-11 10:33:33 +0800

    0.8.0 auto & decltype

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,51 @@
 #include <iostream>
-#include <memory>
-#include "Two.h"
+// #include <memory>
+// #include <string>
+// #include <cctype>
+# include <vector>
+// #include "Two.h"
 
-extern int g_two;
+// extern int g_two;
 
 int main(int argc, char **argv)
 {
-    // Tow class
-    // std::unique_ptr<Two> two(new Two(10, 20));
-    auto two = std::make_unique<Two>(10, 20);
-    two->info();
-    std::cout << "Two: max = " << two->max()
-              << ", g_two: " << g_two
-              << std::endl;
+    // -- vector
+    vector<int> ints;
 
-    // Command line parameters
+
+    // -- auto & decltype
+    // auto m = 123;
+    // decltype(m) n = 456;
+    // std::cout << m + n << std::endl;
+
+    // -- string
+    // std::string line;
+    // while (std::getline(std::cin, line))
+    // {
+    //     std::cout << line << std::endl;
+    // }
+    // std::string s1 = "hello", s2 = "world";
+    // std::string s3 = s1 + s2;
+    // -- error: std::string s4 = "hello" + "world";
+    // std::cout << s3 << std::endl;
+    // unsigned char c = '?';
+    // std::cout << std::ispunct(c) << std::endl;
+
+    // -- Tow class
+    // -- std::unique_ptr<Two> two(new Two(10, 20));
+    // auto two = std::make_unique<Two>(10, 20);
+    // two->info();
+    // std::cout << "Two: max = " << two->max()
+    //           << ", g_two: " << g_two
+    //           << std::endl;
+
+    // -- Command line parameters
     // for (int i = 0; i < argc; i++)
     // {
     //     std::cout << *argv[i] << std::endl;
     // }
 
-    // C++ version
+    // -- C++ version
     // switch (__cplusplus)
     // {
     // case 199711L:
@@ -43,7 +68,7 @@ int main(int argc, char **argv)
     //     break;
     // }
 
-    // Hello World
+    // -- Hello World
     // std::cout << "hello world" << std::endl;
 
     return 0;
