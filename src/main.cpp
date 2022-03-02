82011e51eec8594b2fb2609adc4aae383a918a53
qlhan <qlhan@incarcloud.com>
2022-03-02 16:57:38 +0800

    0.8.0 syntax.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
-// #include <string>
-// #include <cctype>
-// #include <vector>
 #include "syntax.h"
 
 // extern int g_two;
 
 int main(int argc, char **argv)
 {
-
     // -- vector
-    // std::vector<int> v1 = {1, 2, 3};
-    // v1.push_back(4);
-    // if (!v1.empty())
-    // {
-    //     std::cout << v1.size() << std::endl;
-    // }
-    // // -- old: for (std::vector<std::int>::iterator it1 = v1.begin(); it1 != v1.end(); it1++)
-    // for (auto it1 = v1.begin(); it1 != v1.end(); it1++)
-    // {
-    //     std::cout << *it1 << std::endl;
-    // }
-    // const std::vector<std::string> v2(3, "abc");
-    // for (auto it2 = v2.cbegin(); it2 != v2.cend() && !it2->empty(); it2++)
-    // {
-    //     std::cout << *it2 << std::endl;
-    // }
+    test_syntax_vector();
 
     // -- auto & decltype
-    test_syntax_auto();
+    // test_syntax_auto();
 
-    // -- Test string
+    // -- string
     // test_syntax_string();
 
-    // -- Test class
+    // -- class
     // test_syntax_class();
     // std::cout << g_two << std::endl;
 
