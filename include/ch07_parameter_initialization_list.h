94503d5ddc3b7bd845474749ff0703dfdce67a4b
qlhan <qlhan@incarcloud.com>
2023-10-15 14:26:39 +0800

    test: parameter initialization list
    object = single type

--- include/ch07_parameter_initialization_list.h
+++ include/ch07_parameter_initialization_list.h
@@ -6,14 +6,15 @@ class Ch07Entity
 {
   private:
     std::string m_Name;
+    int m_Age;
     mutable int m_DebugCount = 0;
 
   public:
-    Ch07Entity() : m_Name("Unknown")
+    Ch07Entity(int age) : m_Name("Unknown"), m_Age(age)
     {
     }
 
-    Ch07Entity(const std::string &name) : m_Name(name)
+    Ch07Entity(const std::string &name) : m_Name(name), m_Age(-1)
     {
     }
 
@@ -24,12 +25,19 @@ class Ch07Entity
     }
 };
 
+void PrintEntity(const Ch07Entity &entity)
+{
+}
+
 void test_syntax_object()
 {
-    Ch07Entity e0;
+    Ch07Entity e0(18);
     std::cout << e0.GetName() << std::endl;
     Ch07Entity e1("Aaric");
     std::cout << e1.GetName() << std::endl;
+
+    PrintEntity(18);
+    PrintEntity(std::string("Aaric"));
 
     int x = 8;
     auto f = [=]() mutable {
