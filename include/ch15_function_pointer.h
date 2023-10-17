d7e343f93b949ade9c252aa9a4771a67ec582c10
qlhan <qlhan@incarcloud.com>
2023-10-17 19:48:09 +0800

    test: function pointer
    with lambda

--- include/ch15_function_pointer.h
+++ include/ch15_function_pointer.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <functional>
 #include <iostream>
 #include <vector>
 
@@ -33,6 +34,14 @@ void ForEach(const std::vector<int>& values, void (*func)(int))
     }
 }
 
+void ForEach(const std::vector<int>& values, const std::function<void(int)>& func)
+{
+    for (int value : values)
+    {
+        func(value);
+    }
+}
+
 void test_function_pointer()
 {
     //    void (*FunctionName)();
@@ -48,5 +57,16 @@ void test_function_pointer()
 
     std::vector<int> values = {1, 5, 4, 2, 3};
     ForEach(values, PrintValue);
-    ForEach(values, [](int value) { std::cout << "Value: " << value << std::endl; });
+    //    ForEach(values, [](int value) { std::cout << "Value: " << value << std::endl; });
+
+    int age = 18;
+    // auto lambda = [=](int value) { std::cout << "Value: " << age << std::endl; };
+    auto lambda = [=](int value) mutable {
+        age = 35;
+        std::cout << "Value: " << value << ", Age: " << age << std::endl;
+    };
+    ForEach(values, lambda);
+
+    auto it = std::find_if(values.begin(), values.end(), [](int value) { return value > 3; });
+    std::cout << *it << std::endl;
 }
