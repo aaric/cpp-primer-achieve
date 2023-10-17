5d9c279e6afb9b36d2da5273139636568348bf6a
qlhan <qlhan@incarcloud.com>
2023-10-17 20:21:34 +0800

    test: syntax chrono
    with thread

--- include/ch17_chrono.h
+++ include/ch17_chrono.h
@@ -3,13 +3,46 @@
 #include <iostream>
 #include <thread>
 
+class Timer
+{
+private:
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<float> duration;
+
+public:
+    Timer()
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Timer()
+    {
+        end = std::chrono::high_resolution_clock::now();
+
+        duration = end - start;
+        std::cout << "Timer Usage: " << duration.count() << "ms" << std::endl;
+    }
+};
+
+void ForEach()
+{
+    Timer timer;
+
+    for (int i = 0; i < 100; ++i)
+    {
+        std::cout << "Value: " << i << std::endl;
+    }
+}
+
 void test_syntax_chrono()
 {
-    using namespace std::literals::chrono_literals;
-    auto start = std::chrono::high_resolution_clock::now();
-    std::this_thread::sleep_for(1s);
-    auto end = std::chrono::high_resolution_clock::now();
+    // using namespace std::literals::chrono_literals;
+    // auto start = std::chrono::high_resolution_clock::now();
+    // std::this_thread::sleep_for(1s);
+    // auto end = std::chrono::high_resolution_clock::now();
 
-    std::chrono::duration<float> duration = end - start;
-    std::cout << "Working: " << duration.count() << std::endl;
+    // std::chrono::duration<float> duration = end - start;
+    // std::cout << "Working: " << duration.count() << std::endl;
+
+    ForEach();
 }
