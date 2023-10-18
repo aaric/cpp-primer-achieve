e7dd0fd014db03f9c0849b2118fb2e89b883ce7b
qlhan <qlhan@incarcloud.com>
2023-10-18 20:24:56 +0800

    test: algorithm sort
    with sort

--- include/ch19_algorithm_sort.h
+++ include/ch19_algorithm_sort.h
@@ -6,7 +6,18 @@
 void test_algorithm_sort()
 {
     std::vector<int> values = {3, 5, 1, 4, 2};
-    std::sort(values.begin(), values.end(), std::greater<int>());
+    // std::sort(values.begin(), values.end(), std::greater<int>());
+    std::sort(values.begin(), values.end(), [](int a, int b) {
+        if (1 == a)
+        {
+            return true;
+        }
+        if (1 == b)
+        {
+            return false;
+        }
+        return a > b;
+    });
 
     for (int value : values)
     {
